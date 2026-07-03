b7a577ab1df36f0dd66e731fd7bc9752e51327ac
amir-bot-sourse <a67354343@gmail.com>
2026-07-03 18:02:42 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+from telegram.ext import Application, CommandHandler
+from config import BOT_TOKEN
+from handlers.start import start
+
+def create_app():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    return app
